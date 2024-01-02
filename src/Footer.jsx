18e85ce3452bb80b5a6dae612ee681e132f9a365
ej8899 <ej8899@gmail.com>
2024-01-01 21:57:09 -0500

    update: tweak footer details

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,7 +10,6 @@ export default function Ourfooter() {
       <Footer.LinkGroup>
         <Footer.Link href="#">About</Footer.Link>
         <Footer.Link href="#">Privacy Policy</Footer.Link>
-        <Footer.Link href="#">Licensing</Footer.Link>
         <Footer.Link href="#">Contact</Footer.Link>
       </Footer.LinkGroup>
     </Footer>
