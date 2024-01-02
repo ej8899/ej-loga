96bc6f5b859722026ca4da1370153822d857ea56
ej8899 <ej8899@gmail.com>
2024-01-02 06:08:32 -0500

    add: visitor by date chart

--- src/Summaries.jsx
+++ src/Summaries.jsx
@@ -3,36 +3,25 @@
 
 import { Card } from 'flowbite-react';
 import TrafficChart from './TrafficChart';
+import VisitorChart from './VisitorChart';
 
 export default function Summaries() {
   return (
     <div className="flex justify-end space-x-4">
 
+      <Card href="#" className="max-w-sm">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Visits/Day (last 30 days)
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          loreum ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+      </Card>
+
       <TrafficChart/>
 
-      <Card href="#" className="max-w-sm">
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Unique Visitors
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          loreum ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Repeat Visitors
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          loreum ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
-      </Card>
+      <VisitorChart/>
 
-      <Card href="#" className="max-w-sm">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Visits/Day (last 30 days)
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        loreum ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      </Card>
     </div>
   );
 }
