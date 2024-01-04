fb72194e87decbf2df8861b639fdc4842d3642f8
ej8899 <ej8899@gmail.com>
2024-01-04 13:59:01 -0500

    fix: (C) link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ import { Footer } from 'flowbite-react';
 export default function Ourfooter() {
   return (
     <Footer container>
-      <Footer.Copyright href="#" by="ErnieJohnson.ca" year={2024} />
+      <Footer.Copyright href="https://erniejohnson.ca" by="ErnieJohnson.ca" year={2024} />
       <Footer.LinkGroup>
         <Footer.Link href="#">About</Footer.Link>
         <Footer.Link href="#">Privacy Policy</Footer.Link>
