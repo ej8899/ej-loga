1b70c866d106f66dc65443ba8fd7434ecb62c540
ej8899 <ej8899@gmail.com>
2024-01-05 22:10:30 -0500

    add: default data to avoid occassional null bug

--- src/Summaries.jsx
+++ src/Summaries.jsx
@@ -6,8 +6,52 @@ import VisitorChart from './VisitorChart';
 import ServerChart from './ServerChart';
 import LogChart from './LogChart';
 
+const defaultData = {
+  python_version: "3.6.8 (default, Nov 14 2023, 16:29:52) \n[GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]",
+  log_file_size: 1,
+  log_total_entries: 1,
+  log_levels_count: {
+    WARN: 20,
+    INFO: 20,
+    ERROR: 20,
+    FATAL: 20,
+    DEBUG: 10,
+    TRACE: 10
+  },
+  unique_visitors: 1,
+  environment_summary: [
+    {
+      Desktop: 60,
+      Mobile: 40
+    },
+    {
+      MacIntel: 33,
+      iPad: 13,
+      iPhone: 10,
+      Win32: 36,
+      'Linux armv81': 6,
+      'Linux armv8l': 13,
+      'Linux x86_64': 4
+    },
+    {
+      Chrome: 5,
+      Safari: 3
+    }
+  ],
+  date_counts: {
+    "2023-12-30": 1,
+    "2023-12-31": 2,
+    "2024-01-01": 1,
+    "2024-01-02": 2,
+    "2024-01-03": 1,
+    "2024-01-04": 2,
+    "2024-01-05": 1,
+    "2024-01-06": 2
+  }
+};
+
 export default function Summaries() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(defaultData);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,7 +76,7 @@ export default function Summaries() {
 
 
   return (
-    <div className="flex justify-end space-x-4">
+    <div className="flex justify-end space-x-4 justify-center">
 
       <LogChart data={data}/>
       
