10e5883cef68e7a9ce3e185d6feaff7309cf9940
ej8899 <ej8899@gmail.com>
2024-01-05 21:50:29 -0500

    add: API summary fetching and pass props to charts

--- src/Summaries.jsx
+++ src/Summaries.jsx
@@ -1,22 +1,46 @@
 
 'use client';
-
+import { useState, useEffect } from 'react';
 import TrafficChart from './TrafficChart';
 import VisitorChart from './VisitorChart';
 import ServerChart from './ServerChart';
 import LogChart from './LogChart';
 
 export default function Summaries() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://erniejohnson.ca/cgi-bin/log.py?action=fetch&fetch=stats');
+
+        if (response.ok) {
+          const jsonData = await response.json();
+          console.log('fetched summary data')
+          setData(jsonData);
+        } else {
+          console.error('Failed to fetch data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
   return (
     <div className="flex justify-end space-x-4">
 
-      <LogChart/>
+      <LogChart data={data}/>
       
-      <ServerChart/>
+      <ServerChart data={data}/>
 
-      <TrafficChart/>
+      <TrafficChart data={data}/>
 
-      <VisitorChart/>
+      <VisitorChart data={data}/>
 
     </div>
   );
