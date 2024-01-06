c225f2336873b339b2f6d982471a336eef92ebf5
ej8899 <ej8899@gmail.com>
2024-01-06 18:53:14 -0500

    update: legend style

--- src/LogChart.jsx
+++ src/LogChart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 'use client';
 import  { useState, useEffect } from 'react';
@@ -70,7 +71,7 @@ export default function LogChart({data}) {
           top: -2,
         },
       },
-      labels: ["warnings", "info", "errors", "fatal",'debug','trace'],
+      labels: ["warnings", "info", "errors", "fatal&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",'debug','trace'],
       legend: {
         position: "bottom",
         fontFamily: "Poppins, sans-serif",
@@ -131,7 +132,7 @@ export default function LogChart({data}) {
   
     <div className="flex justify-between mb-3">
         <div className="flex justify-center items-center">
-            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">Log Stats</h5>
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">Log Stats ({parseFloat(data.log_file_size/1024/1000).toFixed(2)}MB)</h5>
             <svg data-popover-target="chart-info" data-popover-placement="bottom" className="w-3.5 h-3.5 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white cursor-pointer ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm0 16a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm1-5.034V12a1 1 0 0 1-2 0v-1.418a1 1 0 0 1 1.038-.999 1.436 1.436 0 0 0 1.488-1.441 1.501 1.501 0 1 0-3-.116.986.986 0 0 1-1.037.961 1 1 0 0 1-.96-1.037A3.5 3.5 0 1 1 11 11.466Z"/>
             </svg>
