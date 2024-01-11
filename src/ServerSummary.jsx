f46bc11d73ad72308382acccc88e3932a333492c
ej8899 <ej8899@gmail.com>
2024-01-11 17:12:39 -0500

    update: tweak style

--- src/ServerSummary.jsx
+++ src/ServerSummary.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/prop-types */
 
 'use client';
-import { useState, useEffect } from 'react';
-import { Button } from 'flowbite-react';
+import { Button, Tooltip } from 'flowbite-react';
+
+
+// TODO - if warnings within 24hours - change button to orange w. CAUTION
 
 export default function ServerSummary({data}) {
   const [pythonVersion, osInfo] = data.split('\n');
@@ -9,7 +12,7 @@ export default function ServerSummary({data}) {
   return (
     <div className="flex flex-col justify-center items-center">
     <div className="max-w-screen-2xl w-full">
-      <div className="flex justify-between bg-slate-500 dark:bg-gray-800 rounded-xl shadow-lg p-4 border border-slate-600 rounded-xl shadow-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-white m-1">
+      <div className="flex items-center justify-between rounded-xl shadow-lg p-4 border border-slate-600 rounded-xl shadow-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-white m-1">
         <div className="flex items-center">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gcc/gcc-original.svg" width='54' className='' />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" width='54' className='' />
@@ -19,9 +22,11 @@ export default function ServerSummary({data}) {
           </div>
         </div>
 
-        <Button color="success" pill>
-          Operating
+        <Tooltip content="(no errors within 24hrs)">
+        <Button color="success" pill className="cursor-default">
+          Operating OK
         </Button>
+        </Tooltip>
       </div>
     </div>
   </div>
