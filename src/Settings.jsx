ec2bfa52db5b16054f5e0ba39ad18de509d5d859
ej8899 <ej8899@gmail.com>
2024-02-06 08:38:01 -0500

    update: tweak settings layout to table

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -38,59 +38,73 @@ export default function ServerSettings({ data }) {
         </Accordion.Title>
         <Accordion.Content className="dark:bg-grey" >
           <div className="flex flex-row gap-4 w-full">
-              <label className="relative inline-flex items-center me-5 cursor-pointer">
-                <input
-                  type="checkbox"
-                  value=""
-                  checked={switch1}
-                  className="sr-only peer"
-                  onChange={handleSwitch1Change}
-                />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">email on alerts</span>
-              </label>
-
-              <label className="relative inline-flex items-center me-5 cursor-pointer">
-                <input
-                  type="checkbox"
-                  value=""
-                  checked={switch2}
-                  className="sr-only peer"
-                  onChange={handleSwitch2Change}
-                />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">auto archive events</span>
-              </label>
-
-              <label className="relative inline-flex items-center me-5 cursor-pointer">
-                <input
-                  type="checkbox"
-                  value=""
-                  checked={switch4}
-                  className="sr-only peer"
-                  onChange={handleSwitch4Change}
-                />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">show local time</span>
-              </label>
-
-              
-            </div>
-            <div className="flex flex-row mt-4 gap-4 w-full">
-              <label className="relative inline-flex items-center me-5 cursor-pointer">
+            <table>
+              <tr>
+                <td className='p-2'>Local Settings...</td>
+                <td>
+                  <label className="relative inline-flex items-center me-5 cursor-pointer">
                   <input
                     type="checkbox"
                     value=""
-                    checked={switch3}
+                    checked={switch4}
                     className="sr-only peer"
-                    onChange={handleSwitch3Change}
+                    onChange={handleSwitch4Change}
                   />
-                  <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
-                  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">archive NOW</span>
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">show local time</span>
+                  </label>
+                </td>
+              </tr>
+              <tr>
+                <td className='p-2'>Admin Settings...</td>
+                <td>
+                  <label className="relative inline-flex items-center me-5 cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    checked={switch1}
+                    className="sr-only peer"
+                    onChange={handleSwitch1Change}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">email on alerts</span>
                 </label>
+
+                <label className="relative inline-flex items-center me-5 cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    checked={switch2}
+                    className="sr-only peer"
+                    onChange={handleSwitch2Change}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">auto archive events</span>
+                  </label>   
+                </td>
+              </tr>
+              <tr>
+                <td className='p-2 pr-8'>Server Admin Settings...</td>
+                <td>
+                  <label className="relative inline-flex items-center me-5 cursor-pointer">
+                    <input
+                      type="checkbox"
+                      value=""
+                      checked={switch3}
+                      className="sr-only peer"
+                      onChange={handleSwitch3Change}
+                    />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
+                    <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">archive NOW</span>
+                  </label>
+                </td>
+              </tr>
+            </table>
+                      
             </div>
+
             <div className="flex flex-row justify-end">
-              <Tooltip content="disabled for demo">
+              <Tooltip content="not logged in">
                 <Button disabled>save</Button>
               </Tooltip>
             </div>
